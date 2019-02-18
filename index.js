8ffb8952b15d4f6462df12e2de62c613d772116d
Oğuzhan GÜMÜŞ <40228440+prensmiskin@users.noreply.github.com>
2019-02-18 06:48:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,206 +1,119 @@
-var childProcess = require('child_process');
-
-module.exports = pushDir;
-
-function pushDir(opts) {
-  getLastCommitInfo().then(function(info) {
-    var hash = info.hash;
-    var detachedHead = info.branch === '';
-    var originalBranch = detachedHead ? hash : info.branch;
-
-    var directory = opts.dir;
-    var local = opts.branch + '-' + hash;
-    var remote = opts.remote || 'origin';
-    var remoteBranch = opts.branch;
-    var message = typeof opts.message === 'function' ?
-      opts.message(hash) : opts.message || hash;
-    var allowUnclean = opts['allow-unclean'] || opts.force;
-    var overwriteLocal = opts['overwrite-local'] || opts.force;
-    var cleanup = opts.cleanup === undefined ? false : opts.cleanup;
-    var verbose = opts.verbose;
-
-    Promise.resolve()
-      .then(checkIfClean)
-      .catch(function onUnclean(reason) {
-        return allowUnclean ?
-          console.log('ignoring unclean git...') : Promise.reject(reason);
-      })
-
-      .then(noLocalBranchConflict.bind(null, local))
-      .catch(function onBranchConflict(reason) {
-        return overwriteLocal ?
-          overwriteLocalBranch(local, verbose) : Promise.reject(reason);
-      })
-
-      .then(checkoutOrphanBranch.bind(null, directory, local, verbose))
-      .then(addDir.bind(null, directory, verbose))
-      .then(commitDir.bind(null, directory, message, verbose))
-      .then(pushDirToRemote.bind(null, remote, remoteBranch, verbose))
-      .then(resetBranch.bind(null, originalBranch, detachedHead, verbose))
-      .then(cleanup && deleteLocalBranch.bind(null, local, verbose))
-      .catch(handleError);
-
-  }, handleError);
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" CONTENT="oguzhangumus,Oğuzhan GÜMÜŞ,oguzhan gumus,Oğuzhan GÜMÜŞ,Oğuzhan gümüş,iletişim,Oğuzhan Gümüş instagram , programlama dilleri, yazılım">
+    <meta name="yandex-verification" content="1fc3ecf3a80a0b19" />
+    <meta name="description" content="oguzhan gümüş ">
+    <meta name="Author" content="Oğuzhan GÜMÜŞ, 04bddk@gmail.com">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
 }
 
-/**
- * Tasks
- */
-
-function overwriteLocalBranch(local, verbose) {
-  console.log('will overwite local branch...');
-  return deleteLocalBranch(local, verbose);
+li {
+  float: left;
 }
 
-function checkIfClean() {
-  return expectOutputEmpty(
-    'git status --porcelain',
-    'git not clean'
-  );
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
 }
 
-/**
- * Returns name of current branch or empty string if detached HEAD
- * @return {string} - name of current branch
- */
-function getCurrentBranch(verbose) {
-  return Promise.resolve()
-    .then(execCmd.bind(null,
-      'git',
-      ['symbolic-ref', 'HEAD', '-q'],
-      'problem getting current branch',
-      verbose
-    ))
-    .catch(function() {
-      return '';
-    })
-    .then(function(result) {
-      return result.replace(new RegExp('^refs\/heads\/'), '');
-    });
+li a:hover {
+  background-color: #111;
+}
+</style>
+<style>
+.fa {
+  padding: 5%;
+  font-size: 30px;
+  width: 15%;
+  text-align: center;
+  text-decoration: none;
+  margin: 2%;
 }
 
-function resetBranch(branch, detach, verbose) {
-  var detached = detach ? '--detach' : false;
-  return execCmd(
-    'git',
-    ['checkout', '-f', detached, branch].filter(Boolean),
-    'problem resetting branch',
-    verbose
-  );
+.fa:hover {
+    color: yellow;
 }
 
-function addDir(directory, verbose) {
-  return execCmd(
-    'git',
-    ['--work-tree', directory, 'add', '--all'],
-    'problem adding directory to local branch',
-    verbose
-  );
+
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
 }
 
-function commitDir(directory, message, verbose) {
-  return execCmd(
-    'git',
-    ['--work-tree', directory, 'commit', '-m', message],
-    'problem committing directory to local branch',
-    verbose
-  );
+.fa-google {
+  background: #dd4b39;
+  color: white;
 }
 
-function pushDirToRemote(remote, remoteBranch, verbose) {
-  return execCmd(
-    'git',
-    ['push', remote, 'HEAD:' + remoteBranch, '--force'],
-    'problem pushing local branch to remote',
-    verbose
-  );
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
 }
 
-function checkoutOrphanBranch(directory, branch, verbose) {
-  return execCmd(
-    'git',
-    ['--work-tree', directory, 'checkout', '--orphan', branch],
-    'problem creating local orphan branch',
-    verbose
-  );
+.fa-instagram {
+  background: #125688;
+  color: white;
 }
 
-function deleteLocalBranch(branch, verbose) {
-  return execCmd(
-    'git',
-    ['branch', '-D', branch],
-    'problem deleting local branch',
-    verbose
-  );
+
+
+.fa-github {
+  background: black;
+  color: white;
 }
 
-function noLocalBranchConflict(branch) {
-  return expectOutputEmpty(
-    'git branch --list ' + branch,
-    'local branch with name already exists'
-  );
-}
 
-function getLastCommitInfo() {
-  return Promise
-    .all([
-      getLastCommitHash(),
-      getCurrentBranch()
-    ])
-    .then(function(info) {
-      info = info.map(function(s) { return s.trim(); });
-      return {
-        hash: info[0],
-        branch: info[1]
-      };
-    });
-}
+</style>
+</head>
+<body>
+  <ul>
+  <li><a class="active" href="https://prensmiskin.github.io/oguzhangumus/#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+<div class="card bg-dark text-white">
+  <img src="https://user-images.githubusercontent.com/40228440/52910249-ebe29b00-32a5-11e9-9262-21d7bdd7a607.JPG" class="card-img" alt="...">
+  <div class="card-img-overlay">
+<a href="https://twitter.com/mezbjen" class="fa fa-twitter"></a>
+<a href="mailto:04bddk@gmail.com" class="fa fa-google"></a>
+<a href="https://www.youtube.com/channel/UC6KxxTNdYnJoo-1yoIju8Rw?view_as=subscriber" class="fa fa-youtube"></a>
+<a href="https://www.instagram.com/prens_miskin/" class="fa fa-instagram"></a>
+<a href="https://github.com/prensmiskin" class="fa fa-github"></a>
+<div class="card" style="width: 18rem;">
 
-function getLastCommitHash(verbose) {
-  return execCmd(
-    'git',
-    ['rev-parse', '--short', 'HEAD'],
-    'problem getting last commit hash',
-    verbose
-  );
-}
 
-/**
- * Helpers
- */
+  </div>
+  <p class="text-monospace">Düşündüklerinle Eylemlerin Uyum İçinde Olsun</p>
 
-function handleError(err) {
-  console.error('aborted:', err);
-  process.exit(1);
-}
+</div>
 
-function execCmd(cmd, args, errMessage, verbose) {
-  return new Promise(function(resolve, reject) {
-    verbose && console.log(cmd, args.join(' '));
-    const proc = childProcess.spawn(cmd, args, { stdio: 'pipe' });
-    const stdoutChunks = [];
 
-    proc.stdout.on('data', function(data) {
-      stdoutChunks.push(data);
-      verbose && process.stdout.write(data);
-    });
 
-    proc.stderr.on('data', function(data) {
-      verbose && process.stderr.write(data);
-    });
 
-    proc.on('close', function(code) {
-      verbose && console.log('\n');
-      if (code !== 0) reject(errMessage);
-      else resolve(Buffer.concat(stdoutChunks).toString());
-    });
-  });
-}
-
-function expectOutputEmpty(cmd, errMessage) {
-  return new Promise(function(resolve, reject) {
-    childProcess.exec(cmd, function(error, stdout, stderr) {
-      (error || stdout.length || stderr.length) ? reject(errMessage) : resolve();
-    });
-  });
-}
+</body>
+</html>
